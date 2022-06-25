e6b2650cf51de7155cfd4d10bd0f2779802ec149
Hapsari Putri Larasati <larasati.hapsari99@gmail.com>
2022-06-26 01:34:15 +0700

    AdditionalOperators

--- CS101_Calculator_Hapsari.py
+++ CS101_Calculator_Hapsari.py
@@ -1,13 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
+'''
 Hapsari
-"""
+'''
 def calculate():
     operation = input('''
-+ for addition
-- for subtraction
-* for multiplication
-/ for division
++  for addition
+-  for subtraction
+*  for multiplication
+/  for division
+** for power
+%  for modulo
 
 Please type in the math operation you would like to complete:''')
     try:
@@ -18,16 +19,22 @@ Please type in the math operation you would like to complete:''')
         again()
 
     if operation == '+':
-        print('{} + {} = '.format(num1, num2),num1 + num2)
+        print('{} ditambah {} = '.format(num1, num2),num1 + num2)
 
     elif operation == '-':
-        print('{} - {} = '.format(num1, num2), num1 - num2)
+        print('{} dikurang {} = '.format(num1, num2), num1 - num2)
 
     elif operation == '*' or operation.upper() == 'X':
-        print('{} * {} = '.format(num1, num2), num1 * num2)
+        print('{} dikali {} = '.format(num1, num2), num1 * num2)
 
     elif operation == '/':
-        print('{} / {} = '.format(num1, num2), num1 / num2)
+        print('{} dibagi {} = '.format(num1, num2), num1 / num2)
+        
+    elif operation == '**' or operation == '^':
+        print('{} pangkat {} = '.format(num1, num2), num1 ** num2)
+        
+    elif operation == '%':
+        print('{} modulo {} = '.format(num1, num2), num1 % num2)
 
     else:
         print('You have not typed a valid operator, please run the program again ^^')
@@ -43,7 +50,7 @@ Please type Y for YES or N for NO.
     if calc_again.upper() == 'Y':
         calculate()
     elif calc_again.upper() == 'N':
-        print('See you later.')
+        print('See you later, bye bye ^^')
     else:
         print('You may have typed the wrong key, please read it again carefully ^^')
         again()
